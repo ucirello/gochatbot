2f9470863d307afc574a9507de02a6dfe628bca6
Carlos C <uldericofilho@gmail.com>
2015-11-29 00:01:35 +0100

    Show use case for robots memory

--- rules/regex/rules.go
+++ rules/regex/rules.go
@@ -1,11 +1,22 @@
 package regex
 
-import "cirello.io/gochatbot/bot"
+import (
+	"fmt"
+	"time"
+
+	"cirello.io/gochatbot/bot"
+)
 
 var regexRules = []regexRule{
 	{
 		`{{ .RobotName }} jump`, func(bot bot.Self, msg string) []string {
-			return []string{"{{ .User }}, How high?"}
+			var ret []string
+			ret = append(ret, "{{ .User }}, How high?")
+			lastJumpTS := bot.MemoryRead("jump", "lastJump")
+			ret = append(ret, fmt.Sprint("{{ .User }} (last time I jumped:", lastJumpTS, ")"))
+			bot.MemorySave("jump", "lastJump", fmt.Sprint(time.Now()))
+
+			return ret
 		},
 	},
 }
