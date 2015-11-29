e1f719daee0790811f8f22c6f304396b1f6c309e
Carlos C <uldericofilho@gmail.com>
2015-11-30 00:11:41 +0100

    Add example command in Regex rule which uses its matches

--- rules/regex/rules.go
+++ rules/regex/rules.go
@@ -1,7 +1,10 @@
 package regex
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"time"
 
 	"cirello.io/gochatbot/bot"
@@ -18,6 +21,38 @@ var regexRules = []regexRule{
 			bot.MemorySave("jump", "lastJump", fmt.Sprint(time.Now()))
 
 			return ret
+		},
+	},
+	{
+		`{{ .RobotName }} godoc (.*)`, `search godoc.org and return the first result`,
+		func(bot bot.Self, msg string, matches []string) []string {
+			if len(matches) < 2 {
+				return []string{}
+			}
+
+			resp, err := http.Get(fmt.Sprintf("http://api.godoc.org/search?q=%s", url.QueryEscape(matches[1])))
+			if err != nil {
+				return []string{err.Error()}
+			}
+			defer resp.Body.Close()
+
+			var data struct {
+				Results []struct {
+					Path     string `json:"path"`
+					Synopsis string `json:"synopsis"`
+				} `json:"results"`
+			}
+
+			if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+				return []string{err.Error()}
+			}
+
+			if len(data.Results) == 0 {
+				return []string{"package not found"}
+			}
+
+			return []string{fmt.Sprintf("%s %s/%s", data.Results[0].Synopsis, "http://godoc.org", data.Results[0].Path)}
+
 		},
 	},
 }
